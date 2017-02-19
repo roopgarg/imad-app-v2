e4f2f8102c49d02c2aaa9ac9f725ed7600441d41
roopgarg <roopgarg2012@gmail.com>
2017-02-19 17:41:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send('article-one requested')
+    res.send('article-one requested');
     
 });
 
 app.get('/article-two', function(req,res){
-    res.send('article-two requested')
+    res.send('article-two requested');
     
 });
 
 app.get('/article-three', function(req,res){
-    res.send('article-three requested')
+    res.send('article-three requested');
     
 });
 
