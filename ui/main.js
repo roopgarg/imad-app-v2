450c070b39342b085ab29919bd1e3178edb21836
roopgarg <roopgarg2012@gmail.com>
2017-02-23 14:18:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ console.log('Loaded!');
 //move the image
 var img = document.getElementById('madi');
 img.onclick = function(){
-img.style.marginLeft = '100px';
+   img.style.marginLeft = '100px';
 };
